cdc71125594964491908b5cf12bde32c44482e74
abdulkadercse <kaderhosen2002@gmail.com>
2023-01-30 22:17:40 +0600

    Add New features

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile_financing_app_ui/screens/Splash%20Screen/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(builder: (context, child) {
+      return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: GoogleFonts.ibmPlexSansTextTheme(
+              Theme.of(context).textTheme.apply()
+            )
+
+          ),
+          home: const SplashScreen());
+    });
+  }
+}
